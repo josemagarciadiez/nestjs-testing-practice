722bfdb55b65121801016beb0123b723c6fc2cec
Jose Garcia <jmagarciadiez@gmail.com>
2025-09-03 09:47:14 -0300

    Se agregan test e2e para endpoint "notebooks"

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -3,6 +3,8 @@ import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { App } from 'supertest/types';
 import { AppModule } from './../src/app.module';
+import { CreateNotebookDto } from 'src/notebooks/dto/create-notebook.dto';
+import { Notebook } from 'src/notebooks/entities/notebook.entity';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication<App>;
@@ -21,5 +23,45 @@ describe('AppController (e2e)', () => {
       .get('/')
       .expect(200)
       .expect('Hello World!');
+  });
+
+  describe('/notebooks', () => {
+    const dto: CreateNotebookDto = {
+      title: 'Notebook de prueba E2E',
+      content: 'Testing is haaarrrdd',
+    };
+
+    it('(GET) should return empty array when no results', () => {
+      return request(app.getHttpServer())
+        .get('/notebooks')
+        .expect(200)
+        .expect([]);
+    });
+
+    it('(POST) should create a new record', () => {
+      return request(app.getHttpServer())
+        .post('/notebooks')
+        .send(dto)
+        .expect(201)
+        .expect((res: { body: Notebook }) => {
+          expect(res.body).toHaveProperty('id');
+          expect(res.body.title).toBe(dto.title);
+          expect(res.body.content).toBe(dto.content);
+        });
+    });
+
+    it('(GET) should return and array with the persisted records', () => {
+      return request(app.getHttpServer())
+        .get('/notebooks')
+        .expect(200)
+        .expect((res: { body: Notebook[] }) => {
+          expect(res.body).toHaveLength(1);
+          expect(res.body[0]).toHaveProperty('id');
+          expect(res.body[0]).toHaveProperty('title');
+          expect(res.body[0]).toHaveProperty('content');
+          expect(res.body[0].title).toBe(dto.title);
+          expect(res.body[0].content).toBe(dto.content);
+        });
+    });
   });
 });
